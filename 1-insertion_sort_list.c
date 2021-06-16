c84e2770f0c145de9631a28f11ef9a73e1503083
jhonnjc15 <jhonatanjcca@gmail.com>
2021-06-16 06:47:53 +0000

    Fxing some errors

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *next_node = (*list)->next;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (!((*list)->prev) && !((*list)->next)))
 		return;
 
 	while (next_node != NULL)
