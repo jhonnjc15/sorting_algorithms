a46b48fcfd3b679ea8ce430ceb6c6999719ab00f
jhonnjc15 <jhonatanjcca@gmail.com>
2021-06-16 06:45:31 +0000

    Adding more conditions.

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *next_node = (*list)->next;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	while (next_node != NULL)
